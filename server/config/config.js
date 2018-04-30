92b80d84c2165b136ab7043bc4c09685a51bb051
Mauricio <mauricio.lucero2014@gmail.com>
2018-04-29 21:53:56 -0300

    Cambios config

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,12 @@
 process.env.PORT = process.env.PORT || 8787;
 
 //Entorno
-process.env.NODE_ENV = process.env.NODE_ENV || 'desarrollo';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //DB
 let urlDB;
 
-if ( process.env.NODE_ENV === 'desarrollo') {
+if ( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/pedidosDB';
 } else {
     urlDB = process.env.MONGO_URI;
