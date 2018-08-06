77d495f582258ff90be4a39cf712c84186629b8e
Mauricio <mauricio.lucero@gmail.com>
2018-08-06 00:07:27 -0300

    Se cambio el modelo del domicilio para almacenar la direccion completa para mejores busquedas

--- server/modelo/domicilio.js
+++ server/modelo/domicilio.js
@@ -4,13 +4,11 @@ var Schema = mongoose.Schema;
 var SchemaTypes = mongoose.Schema.Types;
 
 var domicilioSchema = new Schema({
-    calle: { type:String, required:[true,'El nombre de la calle es necesario'] },
-    numero: { type: Number, required:[true,'El numero es necesario'] },
-    localidad: { type:String, required:[true,'La localidad es necesaria'] },
+    direccion: { type: String, required: [true, 'La direccion es necesaria'] },
     latitud: { type: Number },
     longitud: { type: Number },
     fechaAlta: { type: String },
-    fechaBaja: { type: String, default: null}
-},{ collection: 'domicilios' } );
+    fechaBaja: { type: String, default: null }
+}, { collection: 'domicilios' });
 
 module.exports = mongoose.model('Domicilio', domicilioSchema);
